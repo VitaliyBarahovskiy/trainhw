ae9dcecb7e0134b826950f13c8c88d911305c179
vitaliybarahovskiy <vitalikbarahovskii@mail.ru>
2023-01-08 21:06:31 +0300

    HW5 add

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,8 +18,10 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+
+                < Route path="/" element={<Navigate to={"/pre-junior"} />} />
                 {/*<Route ...*/}
-                < Route path="/" element={<PreJunior />} />
+
                 < Route path="/pre-junior" element={<PreJunior />} />
                 < Route path="/junior" element={<Junior />} />
                 < Route path="/junior-plus" element={<JuniorPlus />} />
@@ -30,7 +32,7 @@ function Pages() {
                 {/*<Route ...*/}
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route path="/*" element={<Error404 />} />
+                <Route path="*" element={<Error404 />} />
                 {/*<Route ...*/}
             </Routes>
         </div>
